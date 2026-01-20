86996dad315086c29b105b15c0874764d752405b
buddyosher <buddyosher@gmail.com>
2026-01-20 14:35:04 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import familyRoutes from './routes/family';
 import choreRoutes from './routes/chores';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = Number(process.env.PORT) || 8080;
 
 app.use(cors());
 app.use(express.json());
